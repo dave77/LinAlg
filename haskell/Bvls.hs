2b07a46760cc26417b6836d1d381895325a51725
Dave Wiltshire <dunlucky1@gmail.com>
2014-06-03 15:24:20 +1000

    Binding for Haskell.
    Wrote a wrapper to call bvls from Haskell.

---
+++ haskell/Bvls.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+module Bvls(
+	bvls
+) where
+
+-- Foreign Function Interface Includes
+import Foreign.Ptr
+import Foreign.C.Types
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array
+import System.IO.Unsafe
+
+foreign import ccall unsafe "bvls.h bvls"
+	c_bvls :: CInt -> CInt -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble
+		-> Ptr CDouble -> Ptr CDouble -> IO CInt
+
+convert :: (Fractional b, Real a) => [a] -> [b]
+convert = map realToFrac
+
+bvls :: [[Double]] -> [Double] -> [Double] -> [Double] -> Maybe [Double] 
+bvls a b lb ub = unsafePerformIO $ do
+	x <- mallocArray n
+	rc <-   withArray (convert (concat a))	$ \a' ->
+		withArray (convert b)		$ \b' ->
+		withArray (convert lb)		$ \lb' ->
+		withArray (convert ub)		$ \ub' ->
+		c_bvls m' n' a' b' lb' ub' x
+	let res = case rc of
+		0 -> do
+			tmp <- peekArray n x
+			return (Just $ convert tmp) 
+		_ -> return Nothing
+	lift <- res
+	free x
+	return lift
+	where
+		m' = fromIntegral $ length a
+		n = length $ head a
+		n' = fromIntegral n::CInt
